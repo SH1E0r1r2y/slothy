8f5127676f2e163491d89b50a300423b0f961646
SH1E0r1r2y <sherry12312337@gmail.com>
2025-02-25 16:10:48 +0800

    take out

--- examples/naive/armv7m/curve25519_scalarmult.s
+++ examples/naive/armv7m/curve25519_scalarmult.s
@@ -125,9 +125,11 @@
 	//mov r11,lr //to store lr
 	//push {r11}
 	//mov r11,#0
-	push {r2}
+	//push {r2}
+	//sub sp,#44
+	str r2,[sp,#40]
 
-	sub sp,#28 //can't used in slothy
+	//sub sp,#28 //can't used in slothy
 	//frame address sp,36
 	ldm r2,{r2,r3,r4,r5}
 
@@ -137,7 +139,9 @@
 	umull r7,r12,r3,r0
 	umaal r7,r11,r2,r10
 
-	push {r6,r7}  //@slothy:writes=[stack1,stack2]
+	//push {r6,r7}  //@slothy:writes=[stack1,stack2]
+	//sub sp,#8
+	strd r6,r7,[sp,#4]
 	//frame address sp,44
 
 	umull r8,r6,r4,r0
@@ -160,7 +164,7 @@
 	umaal r12,r6,r4,r0
 	umaal r6,r7,r5,r0
 
-	strd r8,r9,[sp,#8]  //@slothy:writes=[stack3,stack4]
+	strd r8,r9,[sp,#12]  //@slothy:writes=[stack3,stack4]
 
 	mov r9,#0
 	umaal r11,r9,r2,r10
@@ -190,7 +194,9 @@
 	umaal r10,r0,r4,r8
 	umaal r6,r0,r5,r8
 
-	push {r0}  //@slothy:writes=[stack0]
+	//push {r0}  //@slothy:writes=[stack0]
+	//sub sp,#4
+	str r0,[sp,#0]
 	//frame address sp,48
 
 	//_ _ _ _ s 6 10 9| 7 | lr 12 11 _ _ _ _
@@ -244,7 +250,9 @@
 	str r11,[sp,#32+4]  //@slothy:writes=[stack9]
 	umaal r9,r10,r3,r0
 	umaal r10,r6,r4,r0
-	pop {r11}  //@slothy:reads=[stack0]
+	//pop {r11}  //@slothy:reads=[stack0]
+	ldr r11,[sp,#0]
+	//add sp,#4
 	//frame address sp,44
 	umaal r11,r6,r5,r0 // 6=carry for next
 
@@ -270,7 +278,7 @@
 
 	//now reduce
 
-	ldrd r4,r5,[sp,#28] // @slothy:reads=[stack8,stack9]
+	ldrd r4,r5,[sp,#32] // @slothy:reads=[stack8,stack9]
 	movs r3,#38
 	mov r8,#0
 	umaal r4,r8,r3,r12
@@ -280,17 +288,24 @@
 	movs r4,#19
 	mul r8,r8,r4
 
-	pop {r0-r2} //@slothy:reads=[stack1,stack2,stack3]
+	//pop {r0-r2} //@slothy:reads=[stack1,stack2,stack3]
+	ldrd r0,r1,[sp,#4]
+	ldr r2,[sp,#12]
+	//add sp,#12
 	//frame address sp,32
 	umaal r0,r8,r3,r5
 	umaal r1,r8,r3,r9
 	umaal r2,r8,r3,r10
 	mov r9,#38
-	pop {r3,r4}   //@slothy:reads=[stack4,stack5]
+	//pop {r3,r4}   //@slothy:reads=[stack4,stack5]
+	ldrd r3,r4,[sp,#16]
+	//add sp,#8
 	//frame address sp,24
 	umaal r3,r8,r9,r11
 	umaal r4,r8,r9,r6
-	pop {r5,r6}  //@slothy:reads=[stack6,stack7]
+	//pop {r5,r6}  //@slothy:reads=[stack6,stack7]
+	ldrd r5,r6,[sp,#24]
+	//add sp,#32
 	//frame address sp,16
 	umaal r5,r8,r9,lr
 	umaal r6,r8,r9,r7
@@ -512,6 +527,224 @@
 
 .endm
 
+// input/result in (r0-r7)
+// clobbers all other registers
+// cycles: 115
+.macro fe25519_sqr_spa
+	//push {lr}
+	//mov r11,lr //to store lr
+	//push {r11}
+	//mov r11,#0
+	//frame push {lr}
+	//sub sp,sp,#20 //push {r0-r4}
+	//frame address sp,24
+	//mul 01, 00
+	umull r9,r10,r0,r0
+	umull r11,r12,r0,r1
+	adds r11,r11,r11
+	mov lr,#0
+	umaal r10,r11,lr,lr
+	
+	//r9 r10 done
+	//r12 carry for 3rd before col
+	//r11+C carry for 3rd final col
+	
+	//push {r9,r10} //@slothy:writes=[stack0,stack1]
+	strd r9,r10,[sp,#0] //@slothy:writes=[stack0,stack1]
+	//frame address sp,32
+	
+	//mul 02, 11
+	mov r8,#0
+	umaal r8,r12,r0,r2
+	adcs r8,r8,r8
+	umaal r8,r11,r1,r1
+	
+	//r8 done (3rd col)
+	//r12 carry for 4th before col
+	//r11+C carry for 4th final col
+	
+	//mul 03, 12
+	umull r9,r10,r0,r3
+	umaal r9,r12,r1,r2
+	adcs r9,r9,r9
+	umaal r9,r11,lr,lr
+	
+	//r9 done (4th col)
+	//r10+r12 carry for 5th before col
+	//r11+C carry for 5th final col
+	
+	strd r8,r9,[sp,#8] //@slothy:writes=[stack2,stack3]
+	
+	//mul 04, 13, 22
+	mov r9,#0
+	umaal r9,r10,r0,r4
+	umaal r9,r12,r1,r3
+	adcs r9,r9,r9
+	umaal r9,r11,r2,r2
+	
+	//r9 done (5th col)
+	//r10+r12 carry for 6th before col
+	//r11+C carry for 6th final col
+	
+	str r9,[sp,#16] //@slothy:writes=[stack4]
+	
+	//mul 05, 14, 23
+	umull r9,r8,r0,r5
+	umaal r9,r10,r1,r4
+	umaal r9,r12,r2,r3
+	adcs r9,r9,r9
+	umaal r9,r11,lr,lr
+	
+	//r9 done (6th col)
+	//r10+r12+r8 carry for 7th before col
+	//r11+C carry for 7th final col
+	
+	str r9,[sp,#20] //@slothy:writes=[stack5]
+	
+	//mul 06, 15, 24, 33
+	mov r9,#0
+	umaal r9,r8,r1,r5
+	umaal r9,r12,r2,r4
+	umaal r9,r10,r0,r6
+	adcs r9,r9,r9
+	umaal r9,r11,r3,r3
+	
+	//r9 done (7th col)
+	//r8+r10+r12 carry for 8th before col
+	//r11+C carry for 8th final col
+	
+	str r9,[sp,#24] //@slothy:writes=[stack6]
+	
+	//mul 07, 16, 25, 34
+	umull r0,r9,r0,r7
+	umaal r0,r10,r1,r6
+	umaal r0,r12,r2,r5
+	umaal r0,r8,r3,r4
+	adcs r0,r0,r0
+	umaal r0,r11,lr,lr
+	
+	//r0 done (8th col)
+	//r9+r8+r10+r12 carry for 9th before col
+	//r11+C carry for 9th final col
+	
+	//mul 17, 26, 35, 44
+	umaal r9,r8,r1,r7 //r1 is now dead
+	umaal r9,r10,r2,r6
+	umaal r12,r9,r3,r5
+	adcs r12,r12,r12
+	umaal r11,r12,r4,r4
+	
+	//r11 done (9th col)
+	//r8+r10+r9 carry for 10th before col
+	//r12+C carry for 10th final col
+	
+	//mul 27, 36, 45
+	umaal r9,r8,r2,r7 //r2 is now dead
+	umaal r10,r9,r3,r6
+	movs r2,#0
+	umaal r10,r2,r4,r5
+	adcs r10,r10,r10
+	umaal r12,r10,lr,lr
+	
+	//r12 done (10th col)
+	//r8+r9+r2 carry for 11th before col
+	//r10+C carry for 11th final col
+	
+	//mul 37, 46, 55
+	umaal r2,r8,r3,r7 //r3 is now dead
+	umaal r9,r2,r4,r6
+	adcs r9,r9,r9
+	umaal r10,r9,r5,r5
+	
+	//r10 done (11th col)
+	//r8+r2 carry for 12th before col
+	//r9+C carry for 12th final col
+	
+	//mul 47, 56
+	movs r3,#0
+	umaal r3,r8,r4,r7 //r4 is now dead
+	umaal r3,r2,r5,r6
+	adcs r3,r3,r3
+	umaal r9,r3,lr,lr
+	
+	//r9 done (12th col)
+	//r8+r2 carry for 13th before col
+	//r3+C carry for 13th final col
+	
+	//mul 57, 66
+	umaal r8,r2,r5,r7 //r5 is now dead
+	adcs r8,r8,r8
+	umaal r3,r8,r6,r6
+	
+	//r3 done (13th col)
+	//r2 carry for 14th before col
+	//r8+C carry for 14th final col
+	
+	//mul 67
+	umull r4,r5,lr,lr // set 0
+	umaal r4,r2,r6,r7
+	adcs r4,r4,r4
+	umaal r4,r8,lr,lr
+	
+	//r4 done (14th col)
+	//r2 carry for 15th before col
+	//r8+C carry for 15th final col
+	
+	//mul 77
+	adcs r2,r2,r2
+	umaal r8,r2,r7,r7
+	adcs r2,r2,lr
+	
+	//r8 done (15th col)
+	//r2 done (16th col)
+	
+	//msb -> lsb: r2 r8 r4 r3 r9 r10 r12 r11 r0 sp+24 sp+20 sp+16 sp+12 sp+8 sp+4 sp
+	//lr: 0
+	//now do reduction
+	
+	mov r6,#38
+	umaal r0,lr,r6,r2
+	lsl lr,lr,#1
+	orr lr,lr,r0, lsr #31
+	and r7,r0,#0x7fffffff
+	movs r5,#19
+	mul lr,lr,r5
+	
+	//pop {r0,r1} //@slothy:reads=[stack0,stack1]
+	ldrd r0,r1,[sp,#0]
+	//add sp,#8
+	//frame address sp,24
+	umaal r0,lr,r6,r11
+	umaal r1,lr,r6,r12
+	
+	mov r11,r3
+	mov r12,r4
+	
+	//pop {r2,r3,r4,r5} //@slothy:reads=[stack2,stack3,stack4,stack5] 
+	ldrd r2,r3,[sp,#8]
+	//add sp,#16
+	ldrd r4,r5,[sp,#16]
+	//add sp,#24
+	//frame address sp,8
+	umaal r2,lr,r6,r10
+	umaal r3,lr,r6,r9
+	
+	umaal r4,lr,r6,r11
+	umaal r5,lr,r6,r12
+	
+	//pop {r6} //@slothy:reads=[stack6]
+	ldr r6,[sp,#24]
+	//add sp,#28
+	//frame address sp,4
+	mov r12,#38
+	umaal r6,lr,r12,r8
+	add r7,r7,lr
+
+	//pop {pc}
+	//pop {r11}
+
+.endm
+
 .macro fe25519_sqr_many
 	mov r11,lr //to store lr
 	push {r11}
@@ -544,8 +777,11 @@ curve25519_scalarmult:
 
 	// stack layout: xp zp xq zq x0  bitpos lastbit scalar result_ptr r4-r11,lr
 	//               0  32 64 96 128 160    164     168    200        204       = 1216
-    push {r0,r4-r11, lr}
-	
+    sub sp,#28
+	mov r3,sp //set frame pointer?無法，r3 r11 都會用到
+	push {r3}
+	mov r3,#0
+	push {r0,r4-r11, lr}
 slothy_start:
 	mov r10,r2
 	//bl loadm：scalar
@@ -609,7 +845,6 @@ slothy_start:
 	// load scalar bit into r1
 	lsrs r1,r0,#5
 	adds r2,sp,#168
-	
 	ldr r1,[r2,r1,lsl #2]
 	and r4,r0,#0x1f
 	lsrs r1,r1,r4
@@ -663,8 +898,10 @@ slothy_start:
 	push {r0-r7}
 	//frame address sp,272
 	
-	sub sp,sp,#20 
-	fe25519_sqr
+	//ldr r3,[sp,284]
+	sub sp,#28
+	fe25519_sqr_spa
+	add sp,#28
 	push {r0-r7}
 	//frame address sp,304
 	
@@ -674,15 +911,17 @@ slothy_start:
 	push {r0-r7}
 	//frame address sp,336
 	
-	sub sp,sp,#20 
-	fe25519_sqr
+	sub sp,#28
+	fe25519_sqr_spa
+	add sp,#28
 	push {r0-r7}
 	//frame address sp,368
 	
 	mov r1,sp
 	add r2,sp,#64
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#128
 	stm r8,{r0-r7}
 	
@@ -729,8 +968,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#64
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#160
 	stm r8,{r0-r7}
 
@@ -741,8 +981,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#32
 	stm r8,{r0-r7}
 
@@ -753,8 +994,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#96
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	stm sp,{r0-r7}
 
 	mov r8,sp
@@ -777,8 +1019,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#256
+	sub sp,#44
 	fe25519_mul r1,r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#224
 	stm r8,{r0-r7}
 
@@ -846,14 +1089,16 @@ slothy_start:
 	
 	add r1,sp,#96
 	mov r2,sp
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	stm sp,{r0-r7}
 
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#32
 	stm r8,{r0-r7}
 
@@ -866,8 +1111,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#32
 	stm r8,{r0-r7}
 	
@@ -879,8 +1125,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#32
 	stm r8,{r0-r7}
 	
@@ -892,8 +1139,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	stm sp,{r0-r7}
 	//z^(2^20 - 2^0)
 	
@@ -907,9 +1155,10 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
-	add sp,sp,#32
+	add sp,#76
+	//add sp,#12
 	//frame address sp,336
 	//z^(2^40 - 2^0)
 
@@ -919,8 +1168,9 @@ slothy_start:
 
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	add r8,sp,#32
 	stm r8,{r0-r7}
 	
@@ -932,8 +1182,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	stm sp,{r0-r7}
 	
 	// 13751 cycles so far for inversion
@@ -948,9 +1199,10 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	//add sp,#12
-	add sp,sp,#44
+	add sp,#76
+	//add sp,#44
 	//frame address sp,336
 	//z^(2^200 - 2^0)
 	
@@ -962,8 +1214,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#32
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	//z^(2^250 - 2^0)
 	
 	movs r8,#5
@@ -972,8 +1225,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#64
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	stm sp,{r0-r7}
 	//z^(2^255 - 21)
 	
@@ -984,8 +1238,9 @@ slothy_start:
 	
 	mov r1,sp
 	add r2,sp,#96
+	sub sp,#44
 	fe25519_mul r1, r2
-	add sp,#12
+	add sp,#44
 	
 	// now final reduce
 	lsr r8,r7,#31
@@ -1057,6 +1312,7 @@ slothy_end:
 
 	//pop {r0} //meadd
     pop {r4-r11, lr}
+	add sp,#32
 	bx lr
 	
 	// 234 cycles after inversion
