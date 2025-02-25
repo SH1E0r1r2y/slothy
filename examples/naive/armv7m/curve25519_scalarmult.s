028dbe30f9837a2a35ecf1a7292c3c26a900d5a5
SH1E0r1r2y <sherry12312337@gmail.com>
2025-02-25 16:20:12 +0800

    how to load to a fixed address

--- examples/naive/armv7m/curve25519_scalarmult.s
+++ examples/naive/armv7m/curve25519_scalarmult.s
@@ -321,216 +321,7 @@
 // input/result in (r0-r7)
 // clobbers all other registers
 // cycles: 115
-.macro fe25519_sqr 
-	//push {lr}
-	//mov r11,lr //to store lr
-	//push {r11}
-	//mov r11,#0
-	//frame push {lr}
-	//sub sp,sp,#20 //push {r0-r4}
-	//frame address sp,24
-	//mul 01, 00
-	umull r9,r10,r0,r0
-	umull r11,r12,r0,r1
-	adds r11,r11,r11
-	mov lr,#0
-	umaal r10,r11,lr,lr
-	
-	//r9 r10 done
-	//r12 carry for 3rd before col
-	//r11+C carry for 3rd final col
-	
-	push {r9,r10} //@slothy:writes=[stack0,stack1]
-	//frame address sp,32
-	
-	//mul 02, 11
-	mov r8,#0
-	umaal r8,r12,r0,r2
-	adcs r8,r8,r8
-	umaal r8,r11,r1,r1
-	
-	//r8 done (3rd col)
-	//r12 carry for 4th before col
-	//r11+C carry for 4th final col
-	
-	//mul 03, 12
-	umull r9,r10,r0,r3
-	umaal r9,r12,r1,r2
-	adcs r9,r9,r9
-	umaal r9,r11,lr,lr
-	
-	//r9 done (4th col)
-	//r10+r12 carry for 5th before col
-	//r11+C carry for 5th final col
-	
-	strd r8,r9,[sp,#8] //@slothy:writes=[stack2,stack3]
-	
-	//mul 04, 13, 22
-	mov r9,#0
-	umaal r9,r10,r0,r4
-	umaal r9,r12,r1,r3
-	adcs r9,r9,r9
-	umaal r9,r11,r2,r2
-	
-	//r9 done (5th col)
-	//r10+r12 carry for 6th before col
-	//r11+C carry for 6th final col
-	
-	str r9,[sp,#16] //@slothy:writes=[stack4]
-	
-	//mul 05, 14, 23
-	umull r9,r8,r0,r5
-	umaal r9,r10,r1,r4
-	umaal r9,r12,r2,r3
-	adcs r9,r9,r9
-	umaal r9,r11,lr,lr
-	
-	//r9 done (6th col)
-	//r10+r12+r8 carry for 7th before col
-	//r11+C carry for 7th final col
-	
-	str r9,[sp,#20] //@slothy:writes=[stack5]
-	
-	//mul 06, 15, 24, 33
-	mov r9,#0
-	umaal r9,r8,r1,r5
-	umaal r9,r12,r2,r4
-	umaal r9,r10,r0,r6
-	adcs r9,r9,r9
-	umaal r9,r11,r3,r3
-	
-	//r9 done (7th col)
-	//r8+r10+r12 carry for 8th before col
-	//r11+C carry for 8th final col
-	
-	str r9,[sp,#24] //@slothy:writes=[stack6]
-	
-	//mul 07, 16, 25, 34
-	umull r0,r9,r0,r7
-	umaal r0,r10,r1,r6
-	umaal r0,r12,r2,r5
-	umaal r0,r8,r3,r4
-	adcs r0,r0,r0
-	umaal r0,r11,lr,lr
-	
-	//r0 done (8th col)
-	//r9+r8+r10+r12 carry for 9th before col
-	//r11+C carry for 9th final col
-	
-	//mul 17, 26, 35, 44
-	umaal r9,r8,r1,r7 //r1 is now dead
-	umaal r9,r10,r2,r6
-	umaal r12,r9,r3,r5
-	adcs r12,r12,r12
-	umaal r11,r12,r4,r4
-	
-	//r11 done (9th col)
-	//r8+r10+r9 carry for 10th before col
-	//r12+C carry for 10th final col
-	
-	//mul 27, 36, 45
-	umaal r9,r8,r2,r7 //r2 is now dead
-	umaal r10,r9,r3,r6
-	movs r2,#0
-	umaal r10,r2,r4,r5
-	adcs r10,r10,r10
-	umaal r12,r10,lr,lr
-	
-	//r12 done (10th col)
-	//r8+r9+r2 carry for 11th before col
-	//r10+C carry for 11th final col
-	
-	//mul 37, 46, 55
-	umaal r2,r8,r3,r7 //r3 is now dead
-	umaal r9,r2,r4,r6
-	adcs r9,r9,r9
-	umaal r10,r9,r5,r5
-	
-	//r10 done (11th col)
-	//r8+r2 carry for 12th before col
-	//r9+C carry for 12th final col
-	
-	//mul 47, 56
-	movs r3,#0
-	umaal r3,r8,r4,r7 //r4 is now dead
-	umaal r3,r2,r5,r6
-	adcs r3,r3,r3
-	umaal r9,r3,lr,lr
-	
-	//r9 done (12th col)
-	//r8+r2 carry for 13th before col
-	//r3+C carry for 13th final col
-	
-	//mul 57, 66
-	umaal r8,r2,r5,r7 //r5 is now dead
-	adcs r8,r8,r8
-	umaal r3,r8,r6,r6
-	
-	//r3 done (13th col)
-	//r2 carry for 14th before col
-	//r8+C carry for 14th final col
-	
-	//mul 67
-	umull r4,r5,lr,lr // set 0
-	umaal r4,r2,r6,r7
-	adcs r4,r4,r4
-	umaal r4,r8,lr,lr
-	
-	//r4 done (14th col)
-	//r2 carry for 15th before col
-	//r8+C carry for 15th final col
-	
-	//mul 77
-	adcs r2,r2,r2
-	umaal r8,r2,r7,r7
-	adcs r2,r2,lr
-	
-	//r8 done (15th col)
-	//r2 done (16th col)
-	
-	//msb -> lsb: r2 r8 r4 r3 r9 r10 r12 r11 r0 sp+24 sp+20 sp+16 sp+12 sp+8 sp+4 sp
-	//lr: 0
-	//now do reduction
-	
-	mov r6,#38
-	umaal r0,lr,r6,r2
-	lsl lr,lr,#1
-	orr lr,lr,r0, lsr #31
-	and r7,r0,#0x7fffffff
-	movs r5,#19
-	mul lr,lr,r5
-	
-	pop {r0,r1} //@slothy:reads=[stack0,stack1]
-	//frame address sp,24
-	umaal r0,lr,r6,r11
-	umaal r1,lr,r6,r12
-	
-	mov r11,r3
-	mov r12,r4
-	
-	pop {r2,r3,r4,r5} //@slothy:reads=[stack2,stack3,stack4,stack5] 
-	//frame address sp,8
-	umaal r2,lr,r6,r10
-	umaal r3,lr,r6,r9
-	
-	umaal r4,lr,r6,r11
-	umaal r5,lr,r6,r12
-	
-	pop {r6} //@slothy:reads=[stack6]
-	//frame address sp,4
-	mov r12,#38
-	umaal r6,lr,r12,r8
-	add r7,r7,lr
-
-	//pop {pc}
-	//pop {r11}
-
-.endm
-
-// input/result in (r0-r7)
-// clobbers all other registers
-// cycles: 115
-.macro fe25519_sqr_spa
+.macro fe25519_sqr
 	//push {lr}
 	//mov r11,lr //to store lr
 	//push {r11}
@@ -753,8 +544,9 @@
 	push {r8}
 	//frame push {r8,lr}
 0:
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 	
 	ldr r8,[sp,#0]
 	subs r8,r8,#1
@@ -900,7 +692,7 @@ slothy_start:
 	
 	//ldr r3,[sp,284]
 	sub sp,#28
-	fe25519_sqr_spa
+	fe25519_sqr
 	add sp,#28
 	push {r0-r7}
 	//frame address sp,304
@@ -912,7 +704,7 @@ slothy_start:
 	//frame address sp,336
 	
 	sub sp,#28
-	fe25519_sqr_spa
+	fe25519_sqr
 	add sp,#28
 	push {r0-r7}
 	//frame address sp,368
@@ -1003,8 +795,9 @@ slothy_start:
 	add r9,sp,#32
 	fe25519_add r8, r9
 
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 
 	add r8,sp,#192
 	stm r8,{r0-r7}
@@ -1013,8 +806,9 @@ slothy_start:
 	add r9,sp,#32
 	fe25519_sub r8, r9
 
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 	stm sp,{r0-r7}
 
 	mov r1,sp
@@ -1075,15 +869,18 @@ slothy_start:
 	// now we must invert zp
 	add r0,sp,#32
 	ldm r0,{r0-r7}
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 	push {r0-r7}
 	//frame address sp,272
 	
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
-	sub sp,sp,#20 
+	add sp,#28
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 	push {r0-r7}
 	//frame address sp,304
 	
@@ -1104,8 +901,9 @@ slothy_start:
 
 	// current stack: z^(2^9) z^(2^11) x z
 
-	sub sp,sp,#20 
+	sub sp,#28
 	fe25519_sqr
+	add sp,#28
 	push {r0-r7}
 	//frame address sp,336
 
